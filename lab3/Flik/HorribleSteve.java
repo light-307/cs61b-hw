77cc929dd8a38c8ee115a9ba451a7fa98ece8922
light-307 <huxiaochen307@gmail.com>
2021-04-27 15:50:29 +0800

    finish lab3

--- lab3/Flik/HorribleSteve.java
+++ lab3/Flik/HorribleSteve.java
@@ -2,6 +2,7 @@ public class HorribleSteve {
     public static void main(String [] args) throws Exception {
         int i = 0;
         for (int j = 0; i < 500; ++i, ++j) {
+            boolean bug = Flik.isSameNumber(i, j);
             if (!Flik.isSameNumber(i, j)) {
                 throw new Exception(
                         String.format("i:%d not same as j:%d ??", i, j));
--- lab3/IntDList/BuggyIntDList.java
+++ lab3/IntDList/BuggyIntDList.java
@@ -33,17 +33,21 @@ public class BuggyIntDList extends IntDList {
 
         // FIXME: Below code has multiple problems. Debug the code to implement correct functionality.
 
-        // ------ WRITE ADDITIONAL CODE HERE AND ONLY HERE (IF NEEDED) ------
+        if (d1 == null) {
+            return d2;
+        } else if (d2 == null) {
+            return d1;
+        }
 
         // ------------------------------------------------------------------
 
         if (d1.val <= d2.val) {
-            d1.next = sortedMerge(d1, d2.next);   // FIXME: Replace this line (if needed). HINT: Step Into(F7) using debugger and try to figure out what it does.
+            d1.next = sortedMerge(d1.next, d2);   // FIXME: Replace this line (if needed). HINT: Step Into(F7) using debugger and try to figure out what it does.
             d1.next.prev = d1;
             d1.prev = null;
             return d1;
         } else {
-            d2.next = sortedMerge(d1.next, d2);   // FIXME: Replace this line (if needed). HINT: Step Into(F7) using debugger and try to figure out what it does.
+            d2.next = sortedMerge(d1, d2.next);   // FIXME: Replace this line (if needed). HINT: Step Into(F7) using debugger and try to figure out what it does.
             d2.next.prev = d2;
             d2.prev = null;
             return d2;
@@ -66,15 +70,15 @@ public class BuggyIntDList extends IntDList {
             temp = p.prev;
             p.prev = p.next;
             p.next = temp;
-            p = p.next;        // FIXME: Replace this line (if needed). HINT: Use debugger and Java Visualizer to figure out what it does.
+            p = p.prev;        // FIXME: Replace this line (if needed). HINT: Use debugger and Java Visualizer to figure out what it does.
         }
 
         // HINT: What does this if block do? Use Debugger and Java Visualizer to figure out.
         if (temp != null) {
-            // ------ WRITE ADDITIONAL CODE HERE AND ONLY HERE (IF NEEDED) -----
+            back = front;
 
             // -----------------------------------------------------------------
-            front = temp.next;    // FIXME: Replace this line (if needed). HINT: Use debugger and Java Visualizer to figure out what it does.
+            front = temp.prev;    // FIXME: Replace this line (if needed). HINT: Use debugger and Java Visualizer to figure out what it does.
         }
     }
 }
